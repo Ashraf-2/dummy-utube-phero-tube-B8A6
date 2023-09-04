7c7720da664c287517f96b6c284d4f2ec5fb6f76
Ashraf-2 <hasanashraful03@gmail.com>
2023-09-04 18:49:10 +0600

    added blog page

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ const handleLoadCategoryButton = async (catId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${catId}`);
     const data = await response.json();
     console.log('sp data -cat: ', data.data);
+    const x = data.data;
+    const len =x.length;
+    console.log('len: ',len);
+    
 
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = '';
@@ -33,40 +37,57 @@ const handleLoadCategoryButton = async (catId) => {
 
 
     // now write forEach function for eachDataCard.
+    
     eachDataCard.forEach((card) => {
-        //console.log("card: ", card);
-        //console.log("img :", card.thumbnail);
-        //console.log("authors: ", card.authors);
-        const x = card.authors[0];
+            //console.log("card: ", card);
+            //console.log("img :", card.thumbnail);
+            //console.log("authors: ", card.authors);
+            const x = card.authors[0];
 
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <div class="card card-compact w-full bg-base-100 shadow-xl">
-           
-            <img class="w-full max-h-40" src=${card.thumbnail} />
-            
-            <div class="card-body">
-            <div class="flex justify-start gap-2">
-            
-                    <div>
-                        <img class="rounded-full  w-16 h-16" src=${x.profile_picture}/>
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="card card-compact w-full bg-base-100 shadow-xl">
+
+                <img class="w-full max-h-40" src=${card.thumbnail} />
+
+                <div class="card-body">
+                <div class="flex justify-start gap-2">
+
+                        <div>
+                            <img class="rounded-full  w-16 h-16" src=${x.profile_picture}/>
+                        </div>
+
+                        <div>
+                            <h2 class="card-title">${card.title}</h2>
+                            <p>${x.profile_name}</p>
+                            <p>${card.others.views} views</p>
+                        </div>
                     </div>
-            
-                    <div>
-                        <h2 class="card-title">${card.title}</h2>
-                        <p>${x.profile_name}</p>
-                        <p>${card.others.views} views</p>
-                    </div>
-                </div>
 
-            </div>    
+                </div>    
 
-        </div>
-        
-        
-        `
-        cardContainer.appendChild(div);
+            </div>
+
+
+            `
+            cardContainer.appendChild(div);
     })
+    
+    
+    const divElse = document.createElement('div');
+  
+    divElse.innerHTML = `
+    <div>
+        <div class="flex-1 text-center">
+            <img src="./icon.png" alt="error icon">
+        </div> 
+        <h2 class="text-xl text-center">Oops!! Sorry, There is no content here</h2>
+    </div>
+    `
+    if(x<=0){
+    cardContainer.appendChild(divElse)
+    }
+   
 
 }
 
